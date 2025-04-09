6d95883e36dff3a7b4922d2c06436c3f94fe8579
vqru <154732468+vqru@users.noreply.github.com>
2025-04-10 00:21:31 +0530

    Delete api/manga.js

--- api/manga.js
+++
@@ -1,7 +0,0 @@
-export default async (req, res) => {
-  const { title } = req.query;
-  const url = `https://api.mangadex.org/manga?title=${encodeURIComponent(title)}&limit=5&includes[]=cover_art`;
-  const result = await fetch(url);
-  const data = await result.json();
-  res.status(200).json(data);
-};
